c1035b6e3c278830e496b7d55c95729e39cbfdcc
Khoirul Anam <khoirulanam1604@mail.ugm.ac.id>
2024-12-03 04:30:33 +0700

    Repository, Result

--- app/src/main/java/com/khrlanamm/dicodingtales/data/Repository.kt
+++ app/src/main/java/com/khrlanamm/dicodingtales/data/Repository.kt
@@ -1,4 +1,119 @@
 package com.khrlanamm.dicodingtales.data
 
-class Repository {
+import com.khrlanamm.dicodingtales.data.remote.response.UploadResponse
+import com.khrlanamm.dicodingtales.data.remote.response.ListStoryItem
+import com.khrlanamm.dicodingtales.data.remote.response.LoginResponse
+import com.khrlanamm.dicodingtales.data.remote.response.RegisterResponse
+import com.khrlanamm.dicodingtales.data.remote.response.Story
+import com.khrlanamm.dicodingtales.data.remote.retrofit.ApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.HttpException
+
+class Repository private constructor(
+    private val apiService: ApiService
+){
+
+    suspend fun login(email: String, password: String): Result<LoginResponse> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = apiService.login(email, password)
+                if (response.error == false) {
+                    Result.Success(response)
+                } else {
+                    Result.Error(response.message)
+                }
+            } catch (e: Exception) {
+                Result.Error("${e.message}")
+            }
+        }
+    }
+
+
+    suspend fun register(name: String, email: String, password: String): Result<RegisterResponse> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = apiService.register(name, email, password)
+
+                if (!response.error) {
+                    Result.Success(response)
+                } else {
+                    Result.Error(response.message)
+                }
+            } catch (e: HttpException) {
+                Result.Error("${e.message}")
+            }
+        }
+    }
+
+    suspend fun getAllStories(token: String): Result<List<ListStoryItem>> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val token = "Bearer $token"
+                val response = apiService.getStories(token)
+                if (!response.error) {
+                    Result.Success(response.listStory)
+                } else {
+                    Result.Error(response.message)
+                }
+            } catch (e: HttpException) {
+                Result.Error("Http Exception: ${e.message}")
+            } catch (e: Exception) {
+                Result.Error("An error occured: ${e.message}")
+            }
+        }
+    }
+
+    suspend fun getDetail(token: String, id: String): Result<Story> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val token = "Bearer $token"
+                val response = apiService.getDetail(token, id)
+                if (!response.error) {
+                    Result.Success(response.story)
+                } else {
+                    Result.Error(response.message)
+                }
+            } catch (e: HttpException) {
+                Result.Error("Http Exception: ${e.message}")
+            } catch (e: Exception) {
+                Result.Error("An error occured: ${e.message}")
+            }
+        }
+    }
+
+    suspend fun upload(
+        token: String,
+        description: RequestBody,
+        photo: MultipartBody.Part,
+        lat: RequestBody? = null,
+        lon: RequestBody? = null
+    ): Result<UploadResponse> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val token = "Bearer $token"
+                val response = apiService.upload(token, description, photo, lat, lon)
+                if (!response.error) {
+                    Result.Success(response)
+                } else {
+                    Result.Error(response.message)
+                }
+            } catch (e: HttpException) {
+                Result.Error("HTTP Exception: ${e.message}")
+            } catch (e: Exception) {
+                Result.Error("An error occurred: ${e.message}")
+            }
+        }
+    }
+    companion object {
+        @Volatile
+        private var INSTANCE: Repository? = null
+        fun getInstance(
+            apiService: ApiService
+        ): Repository = INSTANCE ?: synchronized(this) {
+            INSTANCE ?: Repository(apiService)
+        }.also { INSTANCE = it }
+    }
 }
