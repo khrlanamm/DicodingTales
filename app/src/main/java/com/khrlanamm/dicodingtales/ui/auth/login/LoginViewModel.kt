75544b232e8c5c8a487a55beccd515083b3f6e44
Khoirul Anam <khoirulanam1604@mail.ugm.ac.id>
2024-12-03 12:58:52 +0700

    Login and register XML

---
+++ app/src/main/java/com/khrlanamm/dicodingtales/ui/auth/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.khrlanamm.dicodingtales.ui.auth.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/khrlanamm/dicodingtales/ui/auth/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.khrlanamm.dicodingtales.ui.auth.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
