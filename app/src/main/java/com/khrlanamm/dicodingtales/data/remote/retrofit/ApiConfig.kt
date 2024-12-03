eb649d107a502f31f736b0713e8432eb1477a133
Khoirul Anam <khoirulanam1604@mail.ugm.ac.id>
2024-12-03 15:33:39 +0700

    Timeout 2 minutes

--- app/src/main/java/com/khrlanamm/dicodingtales/data/remote/retrofit/ApiConfig.kt
+++ app/src/main/java/com/khrlanamm/dicodingtales/data/remote/retrofit/ApiConfig.kt
@@ -2,6 +2,7 @@ package com.khrlanamm.dicodingtales.data.remote.retrofit
 
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,14 +10,20 @@ class ApiConfig {
     companion object {
         fun getApiService(): ApiService {
             val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
                 .build()
+
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://story-api.dicoding.dev/v1/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
+
             return retrofit.create(ApiService::class.java)
         }
     }
